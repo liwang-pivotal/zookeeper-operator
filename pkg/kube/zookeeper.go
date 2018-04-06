83011addf1d747195e13093357e70598fa86eb53
Li Wang <liwang@pivotal.io>
2018-04-06 17:14:01 -0400

    add zk headless service

--- pkg/kube/zookeeper.go
+++ pkg/kube/zookeeper.go
@@ -19,9 +19,15 @@ const (
 
 
 func CreateCluster(cluster spec.ZookeeperCluster, client Kubernetes) error {
-	sts := generateZookeeperStatefulset(cluster)
 
-	err := client.CreateOrUpdateStatefulSet(sts)
+	headlessSVC := generateHeadlessService(cluster)
+	err := client.CreateOrUpdateService(headlessSVC)
+	if err != nil {
+		return err
+	}
+
+	sts := generateZookeeperStatefulset(cluster)
+	err = client.CreateOrUpdateStatefulSet(sts)
 	if err != nil {
 		return err
 	}
@@ -31,8 +37,13 @@ func CreateCluster(cluster spec.ZookeeperCluster, client Kubernetes) error {
 
 func DeleteCluster(cluster spec.ZookeeperCluster, client Kubernetes) error {
 	sts := generateZookeeperStatefulset(cluster)
-
 	err := client.deleteStatefulset(sts)
+	if err != nil {
+		return err
+	}
+
+	headlessSVC := generateHeadlessService(cluster)
+	err = client.deleteService(headlessSVC)
 	if err != nil {
 		return err
 	}
@@ -224,10 +235,132 @@ func (k *Kubernetes) deleteStatefulset(statefulset *appsv1Beta2.StatefulSet) err
 
 func createLabels(cluster spec.ZookeeperCluster) map[string]string {
 	labels := map[string]string{
-		"component": "zookeeper",
-		"creator":   "zookeeper-operator",
-		"role":      "broker",
-		"name":      cluster.ObjectMeta.Name,
+		"app": "zk",
 	}
 	return labels
+}
+
+
+
+func (k *Kubernetes) IfServiceExists(service *v1.Service) (bool, error) {
+	methodLogger := logger.WithFields(log.Fields{
+		"method":    "IfServiceExists",
+		"name":      service.ObjectMeta.Name,
+		"namespace": service.ObjectMeta.Namespace,
+	})
+	namespace := service.ObjectMeta.Namespace
+	svc, err := k.Client.CoreV1().Services(namespace).Get(service.ObjectMeta.Name, k.DefaultOption)
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			methodLogger.Debug("Service dosnt exist")
+			return false, nil
+		} else {
+			methodLogger.WithFields(log.Fields{
+				"error": err,
+			}).Error("Cant get Service INFO from API")
+			return false, err
+		}
+
+	}
+	if len(svc.Name) == 0 {
+		methodLogger.Debug("Service.Name == 0, therefore it dosnt exists")
+		return false, nil
+	}
+	return true, nil
+}
+
+
+func generateHeadlessService(cluster spec.ZookeeperCluster) *v1.Service {
+	labelSelectors := createLabels(cluster)
+
+	objectMeta := metav1.ObjectMeta{
+		Name:        "zk-headless",
+		Labels:      map[string]string{
+			"app": "zk-headless",
+		},
+		Namespace: cluster.ObjectMeta.Namespace,
+	}
+
+	service := &v1.Service{
+		ObjectMeta: objectMeta,
+
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{
+				{
+					Name: "server",
+					Port: 2888,
+				},
+				{
+					Name: "leader-election",
+					Port: 3888,
+				},
+				{
+					Name: "client",
+					Port: 2181,
+				},
+			},
+			ClusterIP: "None",
+			Selector: labelSelectors,
+		},
+	}
+
+	return service
+}
+
+func (k *Kubernetes) CreateOrUpdateService(service *v1.Service) error {
+	methodLogger := logger.WithFields(log.Fields{
+		"method":    "CreateOrUpdateService",
+		"name":      service.ObjectMeta.Name,
+		"namespace": service.ObjectMeta.Namespace,
+	})
+
+	exists, err := k.IfServiceExists(service)
+	if err != nil {
+		methodLogger.WithField("error", err).Error("Error while checking if services exists")
+		return err
+	}
+	if !exists {
+		err = k.createService(service)
+	} else {
+		err = k.updateService(service)
+	}
+	if err != nil {
+		methodLogger.WithField("error", err).Error("Error while creating or updating service")
+	}
+	return err
+}
+
+func (k *Kubernetes) createService(service *v1.Service) error {
+	_, err := k.Client.CoreV1().Services(service.ObjectMeta.Namespace).Create(service)
+	return err
+}
+
+func (k *Kubernetes) updateService(service *v1.Service) error {
+	_, err := k.Client.CoreV1().Services(service.ObjectMeta.Namespace).Update(service)
+	return err
+}
+
+func (k *Kubernetes) deleteService(service *v1.Service) error {
+	methodLogger := logger.WithFields(log.Fields{
+		"method":    "DeleteService",
+		"name":      service.ObjectMeta.Name,
+		"namespace": service.ObjectMeta.Namespace,
+	})
+	exists, err := k.IfServiceExists(service)
+	if err != nil {
+		methodLogger.WithField("error", err).Error("Error while checking if services exists")
+		return err
+	}
+	if exists {
+		err = k.Client.CoreV1().Services(service.ObjectMeta.Namespace).Delete(service.ObjectMeta.Name, &metav1.DeleteOptions{})
+		if err != nil {
+			methodLogger.WithField("error", err).Error("Can delete service")
+			return err
+		}
+	} else {
+		methodLogger.Debug("Trying to delete but Service dosnt exist.")
+
+	}
+	return nil
 }
